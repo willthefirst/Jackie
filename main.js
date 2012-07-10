035a3b5a064e7f193892dd00162527ffc1cff7f8
Will Moritz <willthefirst@Wills-MacBook-Pro.local>
2012-07-10 11:27:12 -0400

    added prototype start 1 scene, nate's js recs

--- main.js
+++ main.js
@@ -1,65 +1,109 @@
 var APP = {
 
 	init: function() {
-		APP.paginate();
+		APP.paginate.init();
 	},
 
 	/* GENERAL Page turning and transitions */
 
-	paginate: function() {
+	paginate: {
 
-		/* Navigation */
-		var $next_btn = $('.btn-next');
-		var $previous_btn = $('.btn-previous');
+		$current: $(),
+		$next: $(),
+		$previous: $(),
 
-		/* Page selectors */
-		var $page_current, $page_next, $page_previous, scene, previous;
+		init: function() {
 
+			var page = APP.paginate;
 
-		var newPage = function(current) {
-				$page_current = current;
+			page.newPage($('.page-current'));
 
-				/* Initiate scene-specific JS */
-				scene = $page_current.attr('class').split(' ')[0].replace("-", "_").toString();
-				APP[scene].start($page_current);
-				$page_next = $page_current.prev();
-				$page_previous = $page_current.next();
-				previous = $page_previous.attr('class').split(' ')[0].replace("-", "_").toString();
-				window.setTimeout(function(){
-					APP[previous].reset($page_previous);
-				}, 1200);
-			};
+			/* Navigation */		
+			$('.btn-next').on('click', page.stepForward);
+			$('.btn-previous').on('click', page.stepBack);
+		},
 
-		var stepForward = function() {
-				$page_current.toggleClass('hidden page-current');
-				$page_next.toggleClass('page-current hidden');
-				newPage($page_next);
-			};
+		newPage: function(current) {
 
-		var stepBack = function() {
-				$page_current.toggleClass('hidden page-current');
-				$page_previous.toggleClass('page-current hidden');
-				newPage($page_previous);
-			};
+			var page = APP.paginate;
 
-		newPage($('.page-current'));
-		$next_btn.on('click', stepForward);
-		$previous_btn.on('click', stepBack);
+			page.$current = current;
+
+			/* Initiate scene-specific JS */
+			var scene = page.$current.attr('class').split(' ')[0].replace("-", "_").toString();
+			APP[scene].start(page.$current);
+
+			page.$next     = page.$current.prev();
+			page.$previous = page.$current.next();
+
+			/* Reset previous page elements */
+			var previous = page.$previous.attr('class').split(' ')[0].replace("-", "_").toString();
+
+			window.setTimeout(function(){
+				APP[previous].reset(page.$previous);
+			}, 1200);
+
+		},
+
+		stepForward: function() {
+
+			var page = APP.paginate;
+
+			page.$current.toggleClass('hidden page-current');
+			page.$next.toggleClass('page-current hidden');
+
+			page.newPage(page.$next);
+
+		},
+
+		stepBack: function() {
+
+			var page = APP.paginate;
+
+			page.$current.toggleClass('hidden page-current');
+			page.$previous.toggleClass('page-current hidden');
+
+			page.newPage(page.$previous);
+
+		}
 	},
 
-	start_1: {
+	/* temp */
+	temp_0: {
 		start: function(current) {
-
+			$('.btn-previous').hide();
 		},
 
 		reset: function(current) {}
 	},
 
-	/* SCENE exposition-2 */
+	/* SCENE start_1 */
+
+	start_1: {
+		start: function(current) {
+			$('.btn-previous').hide();
+
+			var $cloud_1 = current.children('.cloud-1');
+			var $cloud_2 = current.children('.cloud-2');
+			var $left_cloud = current.children('.left-cloud');
+			var $right_cloud = current.children('.right-cloud');
+			var $sky = current.children('.sky');
+
+			current.children($cloud_1, $cloud_2, $left_cloud, $right_cloud, $sky).toggleClass('stop go');
+		},
+
+		reset: function(current) {
+
+		}
+	},
+
+	/* SCENE exposition_2 */
 
 	exposition_2: {
 
 		start: function(current) {
+			$('.btn-previous').show();
+
 			current.children('.cloud').css('-webkit-transition','');
 
 			var $cloud = current.children('.cloud');
@@ -76,6 +120,8 @@ var APP = {
 		}
 	},
 
+	/* SCENE cowsale_3 */
+
 	cowsale_3: {
 
 		start: function(current) {},
@@ -84,20 +130,104 @@ var APP = {
 
 	},
 
-	/* SCENE theft-8 */
+	/* SCENE beantoss_4 */
 
-	theft_8: function() {
+	beantoss_4: {
 
-		var $item_1 = $('.item1');
-		var $item_2 = $('.item2');
-		var $item_3 = $('.item3');
+		start: function(current) {},
 
-		var steal = function(item) {
-				item.addClass('steal');
-			};
+		reset: function(current) {}
 
-		steal($item_1);
+	},
 
+	/* SCENE nextmorning_5 */
+
+	nextmorning_5: {
+
+		start: function(current) {},
+
+		reset: function(current) {}
+
+	},
+
+	/* SCENE beanstalk_6 */
+
+	beanstalk_6: {
+
+		start: function(current) {},
+
+		reset: function(current) {}
+
+	},
+
+	/* SCENE arrival_7 */
+
+	arrival_7: {
+
+		start: function(current) {},
+
+		reset: function(current) {}
+
+	},
+
+	/* SCENE giant_8 */
+
+	giant_8: {
+
+		start: function(current) {
+
+		// temp
+		// var $item_1 = $('.item1');
+		// var $item_2 = $('.item2');
+		// var $item_3 = $('.item3');
+
+		// var steal = function(item) {
+		// 		item.addClass('steal');
+		// 	};
+
+		// steal($item_1);
+
+		},
+
+		reset: function(current) {}
+
+	},
+
+	/* SCENE flee-9 */
+
+	flee_9: {
+
+		start: function(current) {},
+
+		reset: function(current) {}
+
+	},
+
+	chop_10: {
+
+		start: function(current) {},
+
+		reset: function(current) {}
+
+	},
+
+	fallen_11: {
+
+		start: function(current) {
+			$('.btn-next').show();
+
+		},
+
+		reset: function(current) {}
+	},
+
+	end_12: {
+
+		start: function(current) {
+			$('.btn-next').hide();
+		},
+
+		reset: function(current) {}
 	}
 };
 
