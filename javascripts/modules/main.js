eae04f6645523e9e37742ab27d4147c1d232da87
willthefirst <willthefirst@gmail.com>
2012-08-15 09:54:56 -0400

    fixed fb share button

--- javascripts/modules/main.js
+++ javascripts/modules/main.js
@@ -4,6 +4,17 @@ var APP = {
 		APP.clickOrTouch();
 		APP.isStandalone();
 		APP.paginate.init();
+		APP.share();
+	},
+
+	share: function() {
+		(function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s); js.id = id;
+			js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=330725970353334";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
 	},
 
 	events: {
