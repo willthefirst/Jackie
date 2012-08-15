fb85f7c1ed3db6d08d683e8b7bb2669a51c4d489
willthefirst <willthefirst@gmail.com>
2012-08-15 15:30:35 -0400

    before real hosting, taken down sharing temporarily

--- javascripts/modules/main.js
+++ javascripts/modules/main.js
@@ -14,8 +14,7 @@ var APP = {
 			js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=330725970353334";
 			fjs.parentNode.insertBefore(js, fjs);
 		}(document, 'script', 'facebook-jssdk'));
-		var a = function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
-		a();
+		(!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs"));
 	},
 
 	events: {
